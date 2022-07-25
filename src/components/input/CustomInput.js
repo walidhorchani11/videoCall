3b0b02164db79be13f447990991bb8998e2b3b8e
walid horchani <walid.horchani@oyez.fr>
2022-07-26 00:51:04 +0100

    add search

---
+++ src/components/input/CustomInput.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import { TextInput, StyleSheet, View } from 'react-native';
+
+const CustomInput = ({ width, height, icon, onChangeText, placeholder = '', autofocus = false, style = {}, ...rest }) => {
+  return (
+    <View style={{ justifyContent: 'center', alignItems: 'center', backgroundColor: 'yellow' }} >
+      <TextInput
+        style={[styles.TextInputStyle, style]}
+        placeholder={placeholder}
+        onChangeText={onChangeText && onChangeText}
+      />
+    </View>
+  )
+
+}
+
+const styles = StyleSheet.create({
+  TextInputStyle: {
+    backgroundColor: 'white',
+    borderColor: 'red',
+    width: '90%',
+    height: 50,
+    borderWidth: 1,
+    borderRadius: 15,
+    marginVertical: 30,
+    paddingHorizontal: 10
+  }
+})
+
+export default CustomInput;
--- src/screens/contacts/ContactScreen.js
+++ src/screens/contacts/ContactScreen.js
@@ -1,16 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   StyleSheet,
   View
 } from 'react-native';
 
 import ContactList from 'src/components/contacts/ContactList';
+import CustomInput from 'src/components/input/CustomInput';
 
 const ContactScreen = () => {
+  const [search, setSearch] = useState('');
 
   return (
     <View>
-      <ContactList />
+      <CustomInput
+        style={{ borderWidth: 3, borderColor: 'blue' }}
+        placeholder='search contact'
+        onChangeText={(val) => {
+          setSearch(val);
+        }}
+      />
+      <ContactList search={search} />
     </View>
   )
 }
