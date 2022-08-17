be0faa02a1e4f3b8d471b2cf55d210e82f5f9192
walid horchani <walid.horchani@oyez.fr>
2022-08-17 16:30:18 +0100

    encrypted storage + async storage with flipper connect

--- App.js
+++ App.js
@@ -1,18 +1,25 @@
 import React from 'react';
+import RNAsyncStorageFlipper from 'rn-async-storage-flipper';
+
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+RNAsyncStorageFlipper(AsyncStorage);
 import {
   StatusBar,
   SafeAreaView
 } from 'react-native';
 
 import { NavigationStack } from 'navigation/stack/Navigation';
+import { AuthProvider } from 'contexts/AuthContext';
 
 const App = () => {
 
+
   return (
-    <>
+    <AuthProvider>
       <StatusBar barStyle='light-content' />
       <NavigationStack />
-    </>
+    </AuthProvider>
   );
 };
 
